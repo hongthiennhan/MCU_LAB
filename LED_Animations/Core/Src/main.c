f33b1544b94a5113a182dd304717e0d8c81ac576
Thien-Nhan Hong <nhan.hongthien@hcmut.edu.vn>
2025-09-19 17:27:13 +0700

    Add clearNumberOnClock function for enhanced clock display control

--- LED_Animations/Core/Src/main.c
+++ LED_Animations/Core/Src/main.c
@@ -178,6 +178,95 @@ void clearAllClock() {
     HAL_GPIO_WritePin(MATRIX_COL8_GPIO_Port, MATRIX_COL8_Pin, 0);
 }
 
+clearNumberOnClock(uint8_t num) {
+	switch (num) {
+			case 1:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW1_GPIO_Port, MATRIX_ROW1_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW2_GPIO_Port, MATRIX_ROW2_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL2_GPIO_Port, MATRIX_COL2_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL3_GPIO_Port, MATRIX_COL3_Pin, 0);
+				break;
+			case 2:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW1_GPIO_Port, MATRIX_ROW1_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW2_GPIO_Port, MATRIX_ROW2_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL4_GPIO_Port, MATRIX_COL4_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL5_GPIO_Port, MATRIX_COL5_Pin, 0);
+				break;
+			case 3:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW1_GPIO_Port, MATRIX_ROW1_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW2_GPIO_Port, MATRIX_ROW2_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL6_GPIO_Port, MATRIX_COL6_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL7_GPIO_Port, MATRIX_COL7_Pin, 0);
+				break;
+			case 4:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW2_GPIO_Port, MATRIX_ROW2_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW3_GPIO_Port, MATRIX_ROW3_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL7_GPIO_Port, MATRIX_COL7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL8_GPIO_Port, MATRIX_COL8_Pin, 0);
+				break;
+			case 5:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW4_GPIO_Port, MATRIX_ROW4_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW5_GPIO_Port, MATRIX_ROW5_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL7_GPIO_Port, MATRIX_COL7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL8_GPIO_Port, MATRIX_COL8_Pin, 0);
+				break;
+			case 6:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW6_GPIO_Port, MATRIX_ROW6_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW7_GPIO_Port, MATRIX_ROW7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL7_GPIO_Port, MATRIX_COL7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL8_GPIO_Port, MATRIX_COL8_Pin, 0);
+				break;
+			case 7:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW7_GPIO_Port, MATRIX_ROW7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW8_GPIO_Port, MATRIX_ROW8_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL7_GPIO_Port, MATRIX_COL7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL6_GPIO_Port, MATRIX_COL6_Pin, 0);
+				break;
+			case 8:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW7_GPIO_Port, MATRIX_ROW7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW8_GPIO_Port, MATRIX_ROW8_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL5_GPIO_Port, MATRIX_COL5_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL4_GPIO_Port, MATRIX_COL4_Pin, 0);
+				break;
+			case 9:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW7_GPIO_Port, MATRIX_ROW7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW8_GPIO_Port, MATRIX_ROW8_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL3_GPIO_Port, MATRIX_COL3_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL1_GPIO_Port, MATRIX_COL2_Pin, 0);
+				break;
+			case 10:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW7_GPIO_Port, MATRIX_ROW7_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW6_GPIO_Port, MATRIX_ROW6_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL1_GPIO_Port, MATRIX_COL1_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL2_GPIO_Port, MATRIX_COL2_Pin, 0);
+				break;
+			case 11:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW5_GPIO_Port, MATRIX_ROW5_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW4_GPIO_Port, MATRIX_ROW4_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL1_GPIO_Port, MATRIX_COL1_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL2_GPIO_Port, MATRIX_COL2_Pin, 0);
+				break;
+			case 12:
+				clearAllClock();
+				HAL_GPIO_WritePin(MATRIX_ROW3_GPIO_Port, MATRIX_ROW3_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_ROW2_GPIO_Port, MATRIX_ROW2_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL1_GPIO_Port, MATRIX_COL1_Pin, 0);
+				HAL_GPIO_WritePin(MATRIX_COL2_GPIO_Port, MATRIX_COL2_Pin, 0);
+				break;
+		}
+}
+
 void setNumberOnClock(uint8_t num) {
 	switch (num) {
 		case 1:
@@ -440,8 +529,10 @@ int main(void)
 //	  controlTraffic();
 //	  clockControl();
 	  if (counter > 12) counter = 1;
-	  setNumberOnClock(counter++);
-	  HAL_Delay(1000);
+	  setNumberOnClock(counter);
+	  HAL_Delay(750);
+	  clearNumberOnClock(counter++);
+	  HAL_Delay(250);
   }
   /* USER CODE END 3 */
 }
