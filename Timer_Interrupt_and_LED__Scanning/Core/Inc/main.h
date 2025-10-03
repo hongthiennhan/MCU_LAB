98751695afd732a9d689c9eb5ec3df354aabda08
Thien-Nhan Hong <nhan.hongthien@hcmut.edu.vn>
2025-10-03 13:40:23 +0700

    init

---
+++ Timer_Interrupt_and_LED__Scanning/Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_7SEG_C_Pin GPIO_PIN_4
+#define LED_7SEG_C_GPIO_Port GPIOA
+#define LED_7SEG1_CTRL_Pin GPIO_PIN_6
+#define LED_7SEG1_CTRL_GPIO_Port GPIOA
+#define LED_7SEG2_CTRL_Pin GPIO_PIN_7
+#define LED_7SEG2_CTRL_GPIO_Port GPIOA
+#define LED_7SEG_A_Pin GPIO_PIN_0
+#define LED_7SEG_A_GPIO_Port GPIOB
+#define LED_7SEG_B_Pin GPIO_PIN_1
+#define LED_7SEG_B_GPIO_Port GPIOB
+#define LED_7SEG3_CTRL_Pin GPIO_PIN_8
+#define LED_7SEG3_CTRL_GPIO_Port GPIOA
+#define LED_7SEG4_CTRL_Pin GPIO_PIN_9
+#define LED_7SEG4_CTRL_GPIO_Port GPIOA
+#define LED_7SEG_D_Pin GPIO_PIN_4
+#define LED_7SEG_D_GPIO_Port GPIOB
+#define LED_7SEG_E_Pin GPIO_PIN_5
+#define LED_7SEG_E_GPIO_Port GPIOB
+#define LED_7SEG_F_Pin GPIO_PIN_6
+#define LED_7SEG_F_GPIO_Port GPIOB
+#define LED_7SEG_G_Pin GPIO_PIN_7
+#define LED_7SEG_G_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
