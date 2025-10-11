13792ccc59c25f307b5c56be546abbfd997c34f3
Thien-Nhan Hong <nhan.hongthien@hcmut.edu.vn>
2025-10-11 14:48:25 +0700

    feat: update timer logic and adjust display conditions in HAL_TIM_PeriodElapsedCallback

--- Timer_Interrupt_and_LED__Scanning/Core/Src/main.c
+++ Timer_Interrupt_and_LED__Scanning/Core/Src/main.c
@@ -340,31 +340,31 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-volatile uint8_t counter = 20;
+volatile uint16_t counter = 20;
 volatile uint16_t timer = 1000;
 volatile uint8_t num1 = 0;
 volatile uint8_t num2 = 0;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	counter--;
-	if (counter <= 30) {
+	if (counter <= 15) {
 		HAL_GPIO_WritePin(LED_7SEG1_CTRL_GPIO_Port, LED_7SEG1_CTRL_Pin, 0);
 		HAL_GPIO_WritePin(LED_7SEG4_CTRL_GPIO_Port, LED_7SEG4_CTRL_Pin, 1);
 		display7SEG(1);
 	}
-	if (counter <= 20) {
+	if (counter <= 10) {
 		HAL_GPIO_WritePin(LED_7SEG1_CTRL_GPIO_Port, LED_7SEG1_CTRL_Pin, 1);
 		HAL_GPIO_WritePin(LED_7SEG2_CTRL_GPIO_Port, LED_7SEG2_CTRL_Pin, 0);
 		display7SEG(2);
 	}
-	if (counter <= 10) {
-		HAL_GPIO_WritePin(LED_7SEG2_CTRL_GPIO_Port, LED_7SEG1_CTRL_Pin, 1);
-		HAL_GPIO_WritePin(LED_7SEG3_CTRL_GPIO_Port, LED_7SEG1_CTRL_Pin, 0);
+	if (counter <= 5) {
+		HAL_GPIO_WritePin(LED_7SEG2_CTRL_GPIO_Port, LED_7SEG2_CTRL_Pin, 1);
+		HAL_GPIO_WritePin(LED_7SEG3_CTRL_GPIO_Port, LED_7SEG3_CTRL_Pin, 0);
 		display7SEG(3);
 	}
 	if (counter <= 0) {
-		counter = 40;
-		HAL_GPIO_WritePin(LED_7SEG1_CTRL_GPIO_Port, LED_7SEG1_CTRL_Pin, 1);
-		HAL_GPIO_WritePin(LED_7SEG1_CTRL_GPIO_Port, LED_7SEG1_CTRL_Pin, 0);
+		counter = 20;
+		HAL_GPIO_WritePin(LED_7SEG3_CTRL_GPIO_Port, LED_7SEG3_CTRL_Pin, 1);
+		HAL_GPIO_WritePin(LED_7SEG4_CTRL_GPIO_Port, LED_7SEG4_CTRL_Pin, 0);
 		display7SEG(0);
 	}
 }
