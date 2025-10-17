56a8ce9150119b10bede555df195150f36e74e61
Thien-Nhan Hong <nhan.hongthien@hcmut.edu.vn>
2025-10-17 13:25:18 +0700

    fix: correct typo in assert_failed function parameter documentation

--- Timer_Interrupt_and_LED__Scanning/Core/Src/main.c
+++ Timer_Interrupt_and_LED__Scanning/Core/Src/main.c
@@ -387,7 +387,7 @@ void Error_Handler(void)
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
   * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
+  * @param  line: assert_param error linurce number
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
