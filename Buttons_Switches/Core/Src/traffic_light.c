50c2e40e5cd2b6dbcc6eeb6aae59b15d7bc376fb
Thien-Nhan Hong <nhan.hongthien@hcmut.edu.vn>
2025-10-29 19:56:31 +0700

    Refactor LED blinking functions to ensure other LEDs are turned off

--- Buttons_Switches/Core/Src/traffic_light.c
+++ Buttons_Switches/Core/Src/traffic_light.c
@@ -90,27 +90,27 @@ void clearTrafficLight() {
 void blinkRedLed() {
     HAL_GPIO_TogglePin(VLED_RED_GPIO_Port, VLED_RED_Pin);
     HAL_GPIO_TogglePin(HLED_RED_GPIO_Port, HLED_RED_Pin);
-    HAL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
 
+    /* ensure other leds off */
     HAL_GPIO_WritePin(VLED_YELLOW_GPIO_Port, VLED_YELLOW_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(VLED_GREEN_GPIO_Port, VLED_GREEN_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(HLED_YELLOW_GPIO_Port, HLED_YELLOW_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(HLED_GREEN_GPIO_Port, HLED_GREEN_Pin, GPIO_PIN_RESET);
 }
+
 void blinkYellowLed() {
     HAL_GPIO_TogglePin(VLED_YELLOW_GPIO_Port, VLED_YELLOW_Pin);
     HAL_GPIO_TogglePin(HLED_YELLOW_GPIO_Port, HLED_YELLOW_Pin);
-    HAL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
 
     HAL_GPIO_WritePin(VLED_RED_GPIO_Port, VLED_RED_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(VLED_GREEN_GPIO_Port, VLED_GREEN_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(HLED_RED_GPIO_Port, HLED_RED_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(HLED_GREEN_GPIO_Port, HLED_GREEN_Pin, GPIO_PIN_RESET);
 }
+
 void blinkGreenLed() {
     HAL_GPIO_TogglePin(VLED_GREEN_GPIO_Port, VLED_GREEN_Pin);
     HAL_GPIO_TogglePin(HLED_GREEN_GPIO_Port, HLED_GREEN_Pin);
-    HAL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
 
     HAL_GPIO_WritePin(VLED_RED_GPIO_Port, VLED_RED_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(VLED_YELLOW_GPIO_Port, VLED_YELLOW_Pin, GPIO_PIN_RESET);
@@ -121,9 +121,10 @@ void blinkGreenLed() {
 void blinkAllLed() {
     HAL_GPIO_TogglePin(VLED_RED_GPIO_Port, VLED_RED_Pin);
     HAL_GPIO_TogglePin(HLED_RED_GPIO_Port, HLED_RED_Pin);
+
     HAL_GPIO_TogglePin(VLED_YELLOW_GPIO_Port, VLED_YELLOW_Pin);
     HAL_GPIO_TogglePin(HLED_YELLOW_GPIO_Port, HLED_YELLOW_Pin);
+
     HAL_GPIO_TogglePin(VLED_GREEN_GPIO_Port, VLED_GREEN_Pin);
     HAL_GPIO_TogglePin(HLED_GREEN_GPIO_Port, HLED_GREEN_Pin);
-    HAL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
 }
